ff495fef01789a95684cd93ca61c587ff015b4d9
Kudos4U <42659997+BriBuff@users.noreply.github.com>
2018-10-19 20:46:54 -0600

    2nd commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
-var computerChoices = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+var letters = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
 
 var wins = 0;
 var losses = 0;
-var numberGuesses = -9;
+var numberGuesses = 9;
 
 var wins = document.getElementById("wins");
 var losses = document.getElementById("losses");
@@ -10,15 +10,23 @@ var numberGuesses = document.getElementById("numberGuesses");
 var userGuesses = document.getElementById("userGuesses");
 
 document.onekeyup = function(event) {
-    var userGuess = event.key
-    var userChoices = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]; 
-    var computerGuess = computerChoices [Math.floor(Math.random() * computerChoices.length)];
-}
+    var userGuess = event.key 
+    var computerGuess = letters [Math.floor(Math.random() * letters.length)];
 
-wins.textContent = "Wins " + wins;
-losses.textContent = "Losses " + losses;
-numberGuesses.textContent = "Number of guesses left " + numberGuesses;
-userGuesses.textContent = "You guessed " + userGuess;
+    if (userGuess === computerGuess) {
+        wins++;
+    else if (userGuess === 0) {
+        losses++;
+    }
+
+    wins.textContent = "Wins " + wins;
+    losses.textContent = "Losses " + losses;
+    numberGuesses.textContent = "Number of guesses left " + numberGuesses;
+    userGuesses.textContent = "You guessed " + userGuess;       
+
+    };
+};
+
 
 
 
