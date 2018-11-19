e9ea1f49d7a9ec4ded95433ffac1255dc46a8501
Bri Buffington <bri.kudos@gmail.com>
2018-11-18 20:17:29 -0700

    working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,47 @@
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
- 
 var numWins = 0;
 var numlosses = 0;
 var numberGuessesMax = 9;
-var guessLeft, guessLetters
+var arrayguesses = [];
 
 var directions= document.getElementById("directions");
 var wins = document.getElementById("wins");
 var losses = document.getElementById("losses");
 var guesses = document.getElementById("numberGuesses");
 var userGuesses = document.getElementById("userGuesses");
-// userGuesses.innerHTML = guessedLetters.join(",");
+
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
 document.onkeyup = function (event) {
-    var userGuess = event.key;
-    var computerGuess = letters[Math.floor(Math.random() * letters.length)];
-    guessLeft = numberGuessesMax;
-
-    for (guessLeft = 0; guessLeft < 9; guessLeft++) {
-    if (userGuess === computerGuess) {
-        numWins++; 
-    }
-
-    // guessLeft -=1;
-
-    else if (guessLeft === 0) {
+    console.log(event.key);
+    console.log(computerGuess);
+    var letter = event.key;
+    
+    // A user presses a button 
+    // compare event.key to computerGuess
+    if (numberGuessesMax == 0) {
         numlosses++;
+        reset();
+    }
+    else if (event.key === computerGuess){
+        numWins++;
+        reset();
+    } else{
+        numberGuessesMax--;
+        arrayguesses.push(letter);
     }
 
 
     directions.textContent = "";
     wins.textContent = "Wins: " + numWins;
     losses.textContent = "Losses: " + numlosses;
-    guesses.textContent = "Number of guesses left: " + guessLeft;
-    userGuesses.textContent = "You guessed: " + userGuess;
+    guesses.textContent = "Number of guesses left: " + numberGuessesMax;
+    userGuesses.textContent = "You guessed: " + arrayguesses;
 
-    // reset (
-    //     numWins();
-    //     numLosses();
-    //     guessLeft();
-
-    // )
-
-    // Psuedocode: Reset needed. The guess counter needs to go down and not be stationary after one guess. 
-    // The user guesses need to show a string of letters prior to reset. 
-    // Attempts to put any of the above pseudocode made it so that the whole game would freeze. Anything in green is what was tried.
-
+    function reset () {
+        computerGuess = letters[Math.floor(Math.random() * letters.length)];
+        numberGuessesMax = 9;
+        arrayguesses = [];
 };
 
 };
